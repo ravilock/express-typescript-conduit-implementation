5cb972770edabf0506a9263f393131a740f596f5
ravi.leal <ravi.leal@ewally.com.br>
2022-05-27 00:00:54 -0300

    chore: add new eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,47 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es2021: true,
-    },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    root: true,
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    plugins: ['@typescript-eslint'],
+    env: {
+        node: true,
+        es2021: true,
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
     rules: {
-        indent: ['error', 4],
         'linebreak-style': ['error', 'unix'],
         quotes: ['error', 'single'],
-        semi: ['error', 'always'],
+        camelcase: [2, { properties: 'always' }],
+        complexity: ['error', { max: 15 }],
+        '@typescript-eslint/no-var-requires': 0,
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/no-unused-vars': 0,
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+        'no-duplicate-case': 'error',
+        'no-duplicate-imports': 'error',
+        'prettier/prettier': 'error',
+        '@typescript-eslint/ban-types': [
+            'error',
+            {
+                types: {
+                    String: false,
+                    Boolean: false,
+                    Number: false,
+                    Symbol: false,
+                    '{}': false,
+                    Object: false,
+                    object: false,
+                    Function: false,
+                },
+                extendDefaults: true,
+            },
+        ],
     },
 };
